911551a6d12a26b9da07a8703b9b55d592f24926
antoineoriol <antoineoriol1@gmail.com>
2023-02-04 15:49:24 +0100

    schema fixed

--- db/migrate/20230204110318_create_questions.rb
+++ db/migrate/20230204110318_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[7.0]
   def change
     create_table :questions do |t|
-      t.references :users, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :content
 
       t.timestamps
