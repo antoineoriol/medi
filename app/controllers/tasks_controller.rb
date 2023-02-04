d1b088858a5cf84faef4294531cfbdc6cd5305af
gueye tamsir <gueye.tamsir78@gmail.com>
2023-02-04 12:55:12 +0100

    routes and controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,13 @@
+class TasksController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :questions do
+    resources :answers, only: [:new, :create] do
+      resources :tasks, only: [:new, :create]
+      end
+      end
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
