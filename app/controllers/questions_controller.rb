34407880e9135e73b11836fc749f1ecadb51e558
gueye tamsir <gueye.tamsir78@gmail.com>
2023-02-11 16:18:02 +0100

    tasks

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(user_id: current_user.id, content: params[:question][:content])
     if @question.save
       @answer = Answer.create(question_id: @question.id, content: call_client)
-      redirect_to question_answer_path(@question, @answer)
+      redirect_to question_answer_tasks_path(@question, @answer)
     else
       render :new
     end
@@ -20,10 +20,10 @@ class QuestionsController < ApplicationController
     response = client.completions(
       parameters: {
         model: "text-davinci-001",
-        prompt: "#{@question.content}, peux-tu me dire que sous forme de liste de 5 éléments maximum quoi faire en automédicamentation ?",
+        prompt: "#{@question.content}, renvoie moi un array de 5 meilleures remedes de grand mere",
         max_tokens: 100
       })
-    response.dig('choices')&.first&.dig('text')&.split("\n")&.reject(&:empty?)
+    response.dig('choices')&.first&.dig('text')
   end
 
   # def sanitize_and_format(answer)
