989e1a1e8975027ab049453d650ac7dc94f20738
gueye tamsir <gueye.tamsir78@gmail.com>
2023-02-11 11:14:52 +0100

    rejected

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,8 @@ class QuestionsController < ApplicationController
     @question = Question.new(user_id: current_user.id, content: params[:question][:content])
     @question.save
     if @question.save
-      redirect_to new_question_answer_path(@question)
+      @answer = Answer.create(question_id: @question.id, content: sanitize_and_format(call_client))
+      redirect_to question_answer_path(@question, @answer)
     else
       render :new
     end
@@ -11,5 +12,30 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+  end
+
+  private
+
+  def call_client
+    client = OpenAI::Client.new
+    response = client.completions(
+      parameters: {
+        model: "text-davinci-003",
+        prompt: "#{@question.content}, peux-tu me dire que sous forme de liste de 8 éléments maximum quoi faire avec des remèdes de grand-mère ?",
+        max_tokens: 400
+      })
+    response["choices"].map { |c| c["text"] }
+  end
+
+  def sanitize_and_format(answer)
+    text = answer.join(", ")
+    list = text.split("\n")
+    list.delete("")
+    list.map! { |item| "<li class='list-group-item'>#{item}</li>" }
+    list.join("\n")
+  end
+
+  def question_params
+    params.require(:question).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :questions do
-      resources :answers, only: [:new, :create, :show] do
-        resources :tasks, only: [:index, :new, :create]
-      end
-      end
+    resources :answers, only: %i[new create show] do
+      resources :tasks, only: %i[index new create]
+    end
+  end
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
