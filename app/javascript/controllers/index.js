dfc8343c96fd163508ee76798305704865f325b1
gueye tamsir <gueye.tamsir78@gmail.com>
2023-02-18 14:59:08 +0100

    design && update && delete tasks

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import TaskController from "./task_controller"
+application.register("task", TaskController)
