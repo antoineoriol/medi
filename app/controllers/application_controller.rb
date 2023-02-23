a15b18b3e49ad0322c7cf72b9c6de9cd9af61d24
Marcos Suarez <marcossuarezr88@gmail.com>
2023-02-23 19:25:59 +0100

    devise treduction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :set_locale
-end
 
-private
+  private
 
-def set_locale
-  I18n.locale = :fr # Or whatever logic you use to choose.
+  def set_locale
+    I18n.locale = :fr # Or whatever logic you use to choose.
+  end
 end
