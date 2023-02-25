5acab4b8820218a1cb92abadb5678f8e825704ff
Marcos Suarez <marcossuarezr88@gmail.com>
2023-02-25 12:16:20 +0100

    loading fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def set_locale
     I18n.locale = :fr # Or whatever logic you use to choose.
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
