a3c7e20c7fb897b971dc95af7b990ffd25750e22
antoineoriol <antoineoriol1@gmail.com>
2023-02-25 14:26:39 +0100

    add calculation logic

--- app/javascript/controllers/task_controller.js
+++ app/javascript/controllers/task_controller.js
@@ -4,11 +4,30 @@ import { patch } from '@rails/request.js'
 // Connects to data-controller="task"
 export default class extends Controller {
 
+  static targets = [ "card", "score", "result" ]
+
   connect(){
-    console.log('hello from task controller')
+    this.cardsLength = this.cardTarget.children.length
+    const score = Math.round((100 / this.cardsLength)*100) / 100
+    this.scoreTargets.forEach((scoreTarget) => {
+      scoreTarget.innerText = score
+    })
+    this.calculate()
   }
 
   async update(event) {
     await patch(event.target.dataset.url);
+    this.calculate()
+  }
+
+  calculate(){
+    let result = 0;
+    this.scoreTargets.forEach(score => {
+      if (score.closest('.card').querySelector('input').checked) {
+        result += parseInt(score.innerText)
+      }
+    })
+
+    this.resultTarget.innerText = `Nombre de points : ${result}%`
   }
 }
