3ec4439f3309310e91a327aaba8df5971dd360e5
Marcos Suarez <marcossuarezr88@gmail.com>
2023-03-01 00:55:09 +0100

    overlay menu and loading centered

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -8,6 +8,6 @@ export default class extends Controller {
   loading(event) {
     console.log('loading')
     console.log(event.currentTarget)
-    event.currentTarget.parentElement.innerHTML = "<div class='lds-heart'><div></div></div>";
+    event.currentTarget.parentElement.innerHTML = "<div class='lds-heart'><div></div></div><div><h4 class='souffle'>Reprenez Votre Souffle ! Madi est la pour vous ...</h4></div>";
   }
 }
