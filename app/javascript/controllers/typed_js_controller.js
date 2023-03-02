996fdc92d822ba8ff10be19008e6b4ea77cd974b
dexter77 <gueye.tamsir78@gmail.com>
2023-03-02 21:19:25 +0100

    front level-up

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,7 +4,7 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Ton super assistant d’auto-medic. "],
+      strings: ["Ton super assistant d’auto-medi(cation). "],
       typeSpeed: 50,
       loop: true
     })
