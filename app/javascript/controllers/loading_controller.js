bd69ba96107de73b52c14530be1e8149ae4febaa
antoineoriol <antoineoriol1@gmail.com>
2023-03-04 10:11:52 +0100

    change davinci techno and loading design

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -8,6 +8,6 @@ export default class extends Controller {
   loading(event) {
     console.log('loading')
     console.log(event.currentTarget)
-    event.currentTarget.parentElement.innerHTML = "<div class='d-flex align-items'><div class='lds-heart'><p class='loading-text'>Commence par respirer lentement 🍃</p></div></div>";
+    event.currentTarget.parentElement.innerHTML = "<div class='lds-heart'><div></div></div><div><h3 class='text-center'>Commence par respirer lentement 🍃</h3></div>";
   }
 }
